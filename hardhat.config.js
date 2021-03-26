9e6a2ba1594eecd8c9e1cf7653bf36e8bf976f79
Wesley Liu <wesley@naos.finance>
2021-03-26 14:12:11 +0800

    refactor: modify hardhat setting for different version contract compiling

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,6 +17,18 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.17",
+      },
+      {
+        version: "0.6.0",
+      },
+      {
+        version: "0.6.12",
+      },
+    ],
+  }
+}
 
