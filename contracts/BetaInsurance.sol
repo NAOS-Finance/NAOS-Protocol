fa97e2dd8ec45d73e78475cd1d8c01884cc0f016
x64x48 <x64x48@protonmail.com>
2021-10-06 21:13:16 +0800

    Add BetaInsurance vault

---
+++ contracts/BetaInsurance.sol
@@ -0,0 +1,112 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BetaInsurance is ERC20 {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    IERC20 public token;
+
+    uint256 public min = 9500;
+    uint256 public constant max = 10000;
+
+    address public governance;
+
+    event GovernanceUpdated(
+        address governance
+    );
+
+    constructor(
+        address _token,
+        address _governance
+    )
+        public
+        ERC20(
+            "Beta Insurance Token",
+            "Beta Insurance"
+        )
+    {
+        require(_governance != address(0), "BetaInsurance: governance address cannot be 0x0");
+        token = IERC20(_token);
+        governance = _governance;
+    }
+
+    function balance() public view returns (uint256) {
+        return token.balanceOf(address(this));
+    }
+
+    function setMin(uint256 _min) external {
+        require(msg.sender == governance, "!governance");
+        min = _min;
+    }
+
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+        emit GovernanceUpdated(_governance);
+    }
+
+    // Custom logic in here for how much the vault allows to be borrowed
+    // Sets minimum required on-hand to keep small withdrawals cheap
+    function available() public view returns (uint256) {
+        return token.balanceOf(address(this)).mul(min).div(max);
+    }
+
+    function earn() public {
+        uint256 _bal = available();
+        token.safeTransfer(msg.sender, _bal);
+    }
+
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint256 _amount) public {
+        uint256 _pool = balance();
+        uint256 _before = token.balanceOf(address(this));
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+        uint256 _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint256 shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdraw(uint256 _shares) public {
+        uint256 r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint256 b = token.balanceOf(address(this));
+        if (b < r) {
+            uint256 _withdraw = r.sub(b);
+            uint256 _after = token.balanceOf(address(this));
+            uint256 _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        token.safeTransfer(msg.sender, r);
+    }
+
+    function getPricePerFullShare() public view returns (uint256) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+}
