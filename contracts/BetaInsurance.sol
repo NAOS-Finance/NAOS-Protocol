252a367c4d06ce806d129723b1b5906afeb641e9
x64x48 <x64x48@protonmail.com>
2021-10-06 21:22:14 +0800

    Add ERC20Burnable

--- contracts/BetaInsurance.sol
+++ contracts/BetaInsurance.sol
@@ -5,9 +5,10 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BetaInsurance is ERC20 {
+contract BetaInsurance is ERC20, ERC20Burnable {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
